10f212dbef7484d0d428fbe803cf99e89e408d9d
andiandnovee <andiandnovee@gmail.com>
2024-12-24 15:18:03 +0700

    laravel_vue_initial

---
+++ resources/js/components/App.vue
@@ -0,0 +1,12 @@
+<template>
+    <h1 class="text-2xl font-bold text-blue-600">Hello Laravel + Vue + TailwindCSS + Preline!</h1>
+</template>
+
+<script>
+export default {
+    name: 'App'
+}
+</script>
+
+<style scoped>
+</style>
